a63508dbb79a9b288930b0af4db80ccf76f30277
konfiot <arthurtoussaint@free.fr>
2017-05-27 18:44:36 +0200

    Annotation de la courbe

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import graham
 import gen
 import matplotlib.pyplot as plt
+#import scipy
 import statistics
 
 out = []
 for i in range(10, 1000):
 	out.append(statistics.mean([len(graham.graham(gen.gen3(i))) for k in range(100)]))
 
+plt.xlabel("$n$")
+plt.ylabel("$\epsilon_n$")
 plt.plot(range(10, 1000), out)
+
+
 plt.show()
